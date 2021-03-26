13ca68410fe78eefe2b500d520620110b54099d9
Shantam Gautam <39696711+ShantamGautam@users.noreply.github.com>
2021-03-26 19:34:56 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,25 +1,51 @@
-suppressPackageStartupMessages(c(
-        library(shinythemes),
-        library(shiny),
-        library(tm),
-        library(stringr),
-        library(markdown),
-        library(stylo)))
-
-source("./inputCleaner.R")
-final4Data <- readRDS(file="./data/final4Data.RData")
-final3Data <- readRDS(file="./data/final3Data.RData")
-final2Data <- readRDS(file="./data/final2Data.RData")
-
+library(shiny)
 
 shinyServer(function(input, output) {
-        
-        wordPrediction <- reactive({
-                text <- input$text
-                textInput <- cleanInput(text)
-                wordCount <- length(textInput)
-                wordPrediction <- nextWordPrediction(wordCount,textInput)})
-        
-        output$predictedWord <- renderPrint(wordPrediction())
-        output$enteredWords <- renderText({ input$text }, quoted = FALSE)
+  # Default ngram input
+  currentTokens <- ""
+  # Default predicted words
+  wordPredicted1 <- ""
+  wordPredicted2 <- ""
+  wordPredicted3 <- ""
+  wordPredicted4 <- ""
+  wordPredicted5 <- ""
+  
+  ngramInput <- reactive({
+    txt <- input$userTxt
+    
+    # Update currentTokens only if last character is a space
+    if(nchar(txt) == 0 || substr(txt, nchar(txt), nchar(txt)) == " ") {
+      # "<<-" to update object in current session and not just in the function context
+      currentTokens <<- tokenizeInput(txt)
+      #browser()
+    }
+    
+    currentTokens
+  })
+  
+  
+  # Mapping outputs to reactive input
+  wordPredictedVect <- reactive({
+    predictNextWordFast(ngramInput(), dfmModel)
+  })
+  
+  output$wordPredicted1 <- renderText({
+    wordPredictedVect()[1]
+  })
+  
+  output$wordPredicted2 <- renderText({
+    wordPredictedVect()[2]
+  })
+  
+  output$wordPredicted3 <- renderText({
+    wordPredictedVect()[3]
+  })
+  
+  output$wordPredicted4 <- renderText({
+    wordPredictedVect()[4]
+  })
+  
+  output$wordPredicted5 <- renderText({
+    wordPredictedVect()[5]
+  })
 })
